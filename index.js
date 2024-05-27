5699c5509490804c7dd646fa497d63ea0d0db6f4
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-05-28 00:04:46 +0600

    backend code checked

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors({
     origin: [
         'http://localhost:5173', 'http://localhost:5174',
+        'https://hire-echo.web.app',
+        'https://hire-echo.firebaseapp.com'
     ],
     credentials: true
 }));
@@ -47,12 +49,19 @@ const client = new MongoClient(uri, {
 });
 
 
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+
 
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         const allJobsCollection = client.db("hireEchoDB").collection("allJobs");
@@ -69,8 +78,14 @@ async function run() {
             const user = req.body;
             const token = await jwt.sign(user, process.env.API_SECRET_KEY, { expiresIn: "2hr" })
             res
-                .cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none' })
+                .cookie('token', token, cookieOptions)
                 .send({ success: true });
+        })
+
+        app.post("/logout" , async (req , res) => {
+            const user = req.body;
+            console.log("logging out user" , user)
+            res.clearCookie('token' , {  ...cookieOptions  , maxAge : 0}).send({success : true})
         })
 
 
@@ -94,21 +109,21 @@ async function run() {
         })
 
         //post a job api
-        app.post("/addJobs", verifyToken , async (req, res) => {
+        app.post("/addJobs", verifyToken, async (req, res) => {
             const jobData = req.body;
             const result = await allJobsCollection.insertOne(jobData);
             res.send(result);
         })
 
         //single job details api
-        app.get("/jobDetails/:id", verifyToken , async (req, res) => {
+        app.get("/jobDetails/:id", verifyToken, async (req, res) => {
             const query = { _id: new ObjectId(req.params.id) }
             const result = await allJobsCollection.findOne(query);
             res.send(result);
         })
 
         //update job details 
-        app.put("/jobDetailsUpdate/:id", verifyToken , async (req, res) => {
+        app.put("/jobDetailsUpdate/:id", verifyToken, async (req, res) => {
             const jobId = req.params.id;
             const jobDetails = req.body;
             const filter = { _id: new ObjectId(jobId) };
@@ -122,7 +137,7 @@ async function run() {
         })
 
         //delete a job api 
-        app.delete("/myJob/:id" , verifyToken , async (req, res) => {
+        app.delete("/myJob/:id", verifyToken, async (req, res) => {
             const jobId = req.params.id;
             const query = { _id: new ObjectId(jobId) }
             const result = await allJobsCollection.deleteOne(query);
@@ -140,7 +155,7 @@ async function run() {
         //applied jobs related api
 
         // applied job get api 
-        app.get("/appliedJobs" , async (req, res) => {
+        app.get("/appliedJobs", async (req, res) => {
             const userEmail = req.query.email;
             const filterBy = req.query.filterBy;
             let query = {};
@@ -166,7 +181,7 @@ async function run() {
 
 
         //user related api
-        app.get("/jobAppliedCount", verifyToken , async (req, res) => {
+        app.get("/jobAppliedCount", verifyToken, async (req, res) => {
             const userEmail = req.query.email;
             let query = {};
             if (userEmail) {
@@ -193,7 +208,7 @@ async function run() {
         })
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
